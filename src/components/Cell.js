b1b3512ffa45fc4130e650d5f7fb9794b5106544
Idrissi-55 <idrissialamiyassine@gmail.com>
2021-11-16 13:33:43 +0300

    added display styled components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyledCell } from './styles/StyledCell';
-import {TETROMINOS} from '../tetrominos';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell 
-    type={'L'}
-    color = {TETROMINOS['L'].color}
+    <StyledCell
+        type={'L'}
+        color={TETROMINOS['L'].color}
     />
-    
+
 );
 
 export default Cell;
