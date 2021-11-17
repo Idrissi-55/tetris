c80f76958be8e55aa1e64089c730c65731deb65b
Idrissi-55 <idrissialamiyassine@gmail.com>
2021-11-17 14:10:29 +0300

    added empty row case

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,27 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage =>
+            newStage.reduce((acc, row) => {
+                //Check if row contains empty cell
+                if(row.findIndex(cell => cell[0] === 0) === -1) {
+                    console.log("empty row")
+                    setRowsCleared(prev => prev +1);
+                    //We drop the actual row and add a new one on the stage
+                    acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return acc;
+                }
+                //else we just add the row to the stage
+                acc.push(row);
+                return acc;
+            }, [])
+
+
         const updateStage = prevStage => {
             // Clear the stage
             const newStage = prevStage.map(row => 
@@ -25,14 +44,16 @@ export const useStage = (player, resetPlayer) => {
             });
             // I check if there is a collision
             if(player.collided) {
+                console.log('collision !')
                 resetPlayer();
+                return sweepRows(newStage)
             }
 
             return newStage;
         }
 
         setStage(prev => updateStage(prev))
-    }, [player, resetPlayer]);
+    }, [player, resetPlayer, rowsCleared]);
 
     return [stage, setStage];
 }
