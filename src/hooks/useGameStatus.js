4ef7f3173436425e96cfd796184cbdf297e92e13
Idrissi-55 <idrissialamiyassine@gmail.com>
2021-11-19 13:53:27 +0300

    fixed useCallback bug

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,13 +1,14 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 
 export const useGameStatus = rowsCleared => {
     //states
     const [score, setScore] = useState(0);
     const [rows, setRows] = useState(0);
     const [level, setLevel] = useState(0);
+    let linePoints = useMemo(() => {}, []);
+    linePoints = [40, 100, 300, 1200];
 
-    const linePoints = [40, 100, 300, 1200];
-
+    
     //useCallback avoids infinite loops
     const calcScore = useCallback(() => {
         //Check if i have a score to count
